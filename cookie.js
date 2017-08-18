f889284859fc3699cca9671564792eda883efc62
SUH <1067489058@qq.com>
2017-08-18 15:15:07 +0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -8,7 +8,8 @@
 function setCookie( key, value, t ) {
     var oDate = new Date();
     oDate.setDate( oDate.getDate() + t );
-    document.cookie = key + '=' + encodeURI( value ) + ';expires=' + oDate.toGMTString();
+    //document.cookie = key + '=' + encodeURI( value ) + ';expires=' + oDate.toGMTString();
+    document.cookie = key + '=' + encodeURI( value ) + ';expires=' + oDate.toUTCString();
 }
 
 /*
