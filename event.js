15a9e97da6b585a785c1258decf09ce089a87116
SUH <1067489058@qq.com>
2017-09-30 02:38:08 -0500

    实现event事件（兼容处理等）

--- event.js
+++ event.js
@@ -3,72 +3,76 @@
   参考：https://github.com/markyun
 */
 suhong.Event = {
-			// 加载 类似jq的ready
-			readyEvent: function(fn) {
-				if ( document.addEventListener ) {
-					document.addEventListener('DOMContentLoaded', function() {
-						document.removeEventListener('DOMContentLoaded', arguments.callee, false);
-					}, false);
-					fn();
-				} else {
-					document.attachEvent('onreadystatechange', function() {
-						document.detachEvent('onreadystatechange', arguments.callee);
-						fn();
-					});
+	// 加载 类似jq的ready
+	readyEvent: function(fn) {
+		if ( document.addEventListener ) {
+			document.addEventListener('DOMContentLoaded', function() {
+				document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+			}, false);
+			fn();
+		} else {
+			document.attachEvent('onreadystatechange', function() {
+				document.detachEvent('onreadystatechange', arguments.callee);
+				fn();
+			});
+		}
+	},
+	// 绑定
+	addEvent: function(element, type, handler) {
+		// 标准下不加on
+		if ( element.addEventListener ) {
+			element.addEventListener(type, handler, false);
+		} else {
+			element.attachEvent('on'+type, function() {
+				handler.call(element);
+			});
+		}
+	},
+	// 解绑
+	removeEvent: function(element, type, handler) {
+		if ( element.removeEventListener ) {
+			element.removeEventListener(type, handler, false);
+		} else {
+			element.detachEvent(type, function() {
+				handler.call(element);
+			});
+		}
+	},
+	// 阻止冒泡
+	stopPropagation: function(ev) {
+		if ( ev.stopPropagation ) {
+			ev.stopPropagation();
+		} else {
+			ev.cancelBubble = true;
+		}
+	},
+	// 阻止默认事件
+	preventDefault: function(ev) {
+		if ( ev.preventDefault ) {
+			ev.preventDefault();
+		} else {
+			ev.returnValue = false;
+		}
+	},
+	// 获取
+	getTarget: function(ev) {
+		if ( !ev ) {
+			ev = this.getEvent();
+		}
+		return event.target || event.srcElement;
+	},
+	getEvent: function(ev) {
+		var ev = ev || window.event;
+		if ( !ev ) {
+			var c = this.getEvent.caller;
+			while( c ) {
+				ev = c.arguments[0];
+				if ( ev && Event === ev.constructor ) {
+					break;
 				}
-			},
-			// 绑定
-			addEvent: function(element, type, handler) {
-				// 标准下不加on
-				if ( element.addEventListener ) {
-					element.addEventListener(type, handler, false);
-				} else {
-					element.attachEvent('on'+type, function() {
-						handler.call(element);
-					});
-				}
-			},
-			removeEvent: function(element, type, handler) {
-				if ( element.removeEventListener ) {
-					element.removeEventListener(type, handler, false);
-				} else {
-					element.detachEvent(type, function() {
-						handler.call(element);
-					});
-				}
-			},
-			stopPropagation: function(ev) {
-				if ( ev.stopPropagation ) {
-					ev.stopPropagation();
-				} else {
-					ev.cancelBubble = true;
-				}
-			},
-			preventDefault: function(ev) {
-				if ( ev.preventDefault ) {
-					ev.preventDefault();
-				} else {
-					ev.returnValue = false;
-				}
-			},
-			getTarget: function(ev) {
-        if ( !ev ) {
-          ev = this.getEvent();
-        }
-				return event.target || event.srcElement;
-			},
-			getEvent: function(ev) {
-				var ev = ev || window.event;
-				if ( !ev ) {
-					var c = this.getEvent.caller;
-					while( c ) {
-						ev = c.arguments[0];
-						if ( ev && Event === ev.constructor ) {
-							break;
-						}
-						c = c.caller;
-					}
-				}
-				return ev;
+				c = c.caller;
 			}
-		};
+		}
+		return ev;
+	}
+};
