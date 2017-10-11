3d93266a26a14a1edca5b51e25059e3c17624a6a
SUH <1067489058@qq.com>
2017-10-11 04:39:40 -0500

    Number.isFinite方法

---
+++ Number.isFinite.js
@@ -0,0 +1,15 @@
+/*
+  ES5实现ES6的Number.isFinite方法
+*/
+(function(global) {
+  var global_isFinite = global.isFinite;
+  
+  Object.defineProperty(Number, 'isFinite', {
+    value: function isFinite(value) {
+      return typeof value === 'number' && global_isFinite(value);
+    },
+    configurable: true,
+    enumerable: false
+  });
+  
+})(this);
