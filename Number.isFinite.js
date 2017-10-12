b27cfe49bf1de351591d4cd4187be61311bb3dbc
SUH <1067489058@qq.com>
2017-10-11 20:16:47 -0500

    isFinite, isInteger

--- Number.isFinite.js
+++ Number.isFinite.js
@@ -1,5 +1,8 @@
 /*
   ES5实现ES6的Number.isFinite, isInteger方法
+  为什么ES6会将增加Number.parseInt、Number.parseFloat?
+  1、使js这门语言趋于完善
+  2、实现模块化，逐渐减少全局变量
 */
 (function(global) {
   var global_isFinite = global.isFinite;
