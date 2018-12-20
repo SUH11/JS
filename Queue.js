8582dd9b43c5de46030c0e8c03118a76edc7f17b
suhong <1067489058@qq.com>
2018-12-20 14:48:37 +0800

    队列

---
+++ Queue.js
@@ -0,0 +1,40 @@
+function Queue() {
+	this.dataStore = [];
+	this.enqueue = enqueue;
+	this.dequeue = dequeue;
+	this.front = front;
+	this.back = back;
+	this.toString = toString;
+	this.empty = empty;
+}
+
+function enqueue(element) {
+	this.dataStore.push(element);
+}
+
+function dequeue() {
+	this.dataStore.shift();
+}
+
+function front() {
+	return this.dataStore[0];
+}
+
+function back() {
+	return this.dataStore[this.dataStore.length - 1];
+}
+
+function toString() {
+	var str = '';
+	for (var i = 0; i < this.dataStore.length; i ++) {
+		str += this.dataStore[i] + '\n';
+	}
+	return str;
+}
+
+function empty() {
+	if (this.dataStore.length === 0) {
+		return true;
+	}
+	return false;
+}
