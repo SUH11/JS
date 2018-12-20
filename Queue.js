60b5f68aa3d73a0127859035fe314f3022c5d538
suhong <1067489058@qq.com>
2018-12-20 15:06:27 +0800

    优先级队列

--- Queue.js
+++ Queue.js
@@ -12,8 +12,19 @@ function enqueue(element) {
 	this.dataStore.push(element);
 }
 
+// function dequeue() {
+// 	this.dataStore.shift();
+// }
+
+// 优先级队列,用code来标识优先级 越小优先级越高
 function dequeue() {
-	this.dataStore.shift();
+	var entry = 0;
+	for (var i = 0; i < this.dataStore.length; i ++) {
+		if (this.dataStore[i].code < this.dataStore[entry].code) {
+			entry = i;
+		}
+	}
+	return this.dataStore.splice(entry, 1);
 }
 
 function front() {
@@ -24,6 +35,7 @@ function back() {
 	return this.dataStore[this.dataStore.length - 1];
 }
 
+// 优先级队列的toString也要改
 function toString() {
 	var str = '';
 	for (var i = 0; i < this.dataStore.length; i ++) {
@@ -38,3 +50,18 @@ function empty() {
 	}
 	return false;
 }
+
+// 优先级队列例子
+function Patient(name, code) {
+	this.name = name;
+	this.code = code;
+}
+
+var p = new Patient('suhong', 5);
+var ed = new Queue();
+ed.enqueue(p);
+p = new Patient('suhong1', 1);
+ed.enqueue(p);
+
+console.log(ed.dequeue()); // suhong1
+
